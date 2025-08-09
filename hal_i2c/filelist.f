85e38de22658cb40f57f83d04a516a9fb5dcab11
Agustin Martino <amartino@fi.uba.ar>
2025-08-09 17:17:19 -0300

    Add RTL, testbench and scripts to run simulation and LINT

---
+++ hal_i2c/filelist.f
@@ -0,0 +1,3 @@
+../i2cslave/rtl/i2cSlave.v
+../i2cslave/rtl/registerInterface.v
+../i2cslave/rtl/serialInterface.v
---
+++ xrun_i2c/filelist.f
@@ -0,0 +1,23 @@
+../i2cslave/rtl/i2cSlave.v
+../i2cslave/rtl/registerInterface.v
+../i2cslave/rtl/serialInterface.v
+../i2cslave/model/i2c_master_bit_ctrl.v
+../i2cslave/model/i2c_master_byte_ctrl.v
+../i2cslave/model/i2c_master_top.v
+../i2cslave/model/wb_master_model.v
+../i2cslave/bench/multiByteReadWrite.v
+../i2cslave/bench/testHarness.v
+../i2cslave/bench/testCase0.v
+
+-incdir ../i2cslave/rtl/
+-incdir ../i2cslave/bench/
+-incdir ../i2cslave/model/
+
+
+
+
+
+
+
+
+
