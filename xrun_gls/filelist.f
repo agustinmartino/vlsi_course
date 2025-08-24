c5e3ffce339e7829656460b121ae3682914f3bdd
Agustin Martino <amartino@fi.uba.ar>
2025-08-24 18:41:00 -0300

    Add scripts for GLS and folder for SDF files

---
+++ xrun_gls/filelist.f
@@ -0,0 +1,17 @@
+/home/amartino/sintesis/syn/outputs_Aug24-17:02:16/i2cSlave_m.v
+
+/home/amslib/PDKs/gpdk045v6/gsclib045/verilog/slow_vdd1v0_basicCells.v
+/home/amslib/PDKs/gpdk045v6/gsclib045_hvt/verilog/slow_vdd1v0_basicCells_hvt.v
+/home/amslib/PDKs/gpdk045v6/gsclib045_lvt/verilog/slow_vdd1v0_basicCells_lvt.v
+
+../i2cslave/model/i2c_master_bit_ctrl.v
+../i2cslave/model/i2c_master_byte_ctrl.v
+../i2cslave/model/i2c_master_top.v
+../i2cslave/model/wb_master_model.v
+../i2cslave/bench/multiByteReadWrite.v
+../i2cslave/bench/testHarness.v
+../i2cslave/bench/testCase0.v
+
+-incdir ../i2cslave/rtl/
+-incdir ../i2cslave/bench/
+-incdir ../i2cslave/model/
